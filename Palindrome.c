028955b71df9c63efdf55b891eae56f0dadcf0e5
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-02-20 17:43:42 +0530

    Create Palindrome.c

---
+++ Palindrome.c
@@ -0,0 +1,16 @@
+int main()
+int num,rev num,rev num,rem,temp num;
+printf("enter the integer");
+scanf("%d",&num);
+temp num=num
+while(temp num!=0)
+{
+rem=temp num%10
+rev num=rev num*10+rem;
+temp num=10
+if(rev num=num)
+printf("palindrome")
+else
+printf("not palindrome")
+return 0;
+}
